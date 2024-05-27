be94c05cf1f6d023ba5d08a8890bc69d64f1fb75
alchimic <alchimicus@gmail.com>
2024-05-27 23:52:04 +0300

    modify script

---
+++ src/js/lib/burgerMenu.js
@@ -0,0 +1,33 @@
+// navigation
+const burger = document.getElementById('burger');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('body');
+
+const openBurger = () => {
+  body.classList.toggle('body-freeze');
+  burger.classList.toggle('burger-active');
+  nav.classList.toggle('nav-open');
+};
+
+const closeBurger = () => {
+  body.classList.remove('body-freeze');
+  burger.classList.remove('burger-active');
+  nav.classList.remove('nav-open');
+};
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    openBurger();
+  });
+
+  document.addEventListener('click', (event) => {
+    if (
+      // event.target.matches('.header__menu-list a') ||
+      event.target.closest('.header__menu-list a')
+    ) {
+      closeBurger();
+    }
+  });
+}
+
+export default 'burgerMenu';
--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,9 @@
 import "../scss/style.scss";
 
-import sayHello from './lib/sayHello.js';
+import burgerMenu from './lib/burgerMenu.js';
 import modal from './lib/modal.js';
+import sayHello from './lib/sayHello.js';
 
 sayHello();
 
-// navigation
-const burger = document.getElementById('burger');
-const nav = document.querySelector('.nav');
-const body = document.querySelector('body');
 
-const openBurger = () => {
-  body.classList.toggle('body-freeze');
-  burger.classList.toggle('burger-active');
-  nav.classList.toggle('nav-open');
-};
-
-const closeBurger = () => {
-  body.classList.remove('body-freeze');
-  burger.classList.remove('burger-active');
-  nav.classList.remove('nav-open');
-};
-
-burger.addEventListener('click', () => {
-  openBurger();
-});
-
-document.addEventListener('click', (event) => {
-  if (
-    // event.target.matches('.header__menu-list a') ||
-    event.target.closest('.header__menu-list a')
-  ) {
-    closeBurger();
-  }
-});
